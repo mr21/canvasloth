cc06316547422dc7103e88a2d16470ca32ce329d
Mr21 <thomastortorini@gmail.com>
2014-02-15 20:39:06 -0500

    JS, Anims, la partie qui gere les animations se detache du fichier Assets.js

---
+++ Assets/Anims.js
@@ -0,0 +1,70 @@
+Assets.prototype.Anims = function(time, sprites) {
+	this.time = time;
+	this.sprites = sprites;
+};
+
+Assets.prototype.Anims.prototype = {
+	create: function(imgPath, x, y, w, h, nbFrames, returnTo, loop, delay) {
+		var an = new this.Anim();
+		an.time = this.time;
+		an.sprite = this.sprites.create(imgPath, x, y, w, h);
+		an.framesAxeX = nbFrames > 0;
+		an.nbFrames = Math.abs(nbFrames);
+		an.returnTo = returnTo;
+		an.loop = loop;
+		an.delay = delay / an.nbFrames;
+		an.frame = an.returnTo === -1 ? -1 : 0;
+		an.timePrev = an.time.realTime;
+		an.pause();
+		return an;
+	}
+};
+
+Assets.prototype.Anims.prototype.Anim = function() {};
+Assets.prototype.Anims.prototype.Anim.prototype = {
+	draw: function(x, y, debug) {
+		if (this.frame > -1) {
+			this.sprite.draw(x, y, debug);
+			this.update(this.time);
+		}
+	},
+	play: function() {
+		this.playing = true;
+		if (this.frame === -1)
+			this.moveFrame(+1);
+	},
+	pause: function() {
+		this.playing = false;
+	},
+	stop: function() {
+		this.playing = false;
+		this.moveFrame(-this.frame - (this.returnTo === -1));
+	},
+	rewind: function() {
+	},
+	// private
+	moveFrame: function(f) {
+		this.frame += f;
+		if (this.framesAxeX)
+			this.sprite.x += this.sprite.w * f;
+		else
+			this.sprite.y += this.sprite.h * f;
+	},
+	update: function(time) {
+		if (!this.playing) {
+			this.timePrev = time.realTime;
+		} else {
+			var nbDelay = Math.floor((time.realTime - this.timePrev) / this.delay);
+			if (nbDelay >= 1) {
+				if (this.frame + nbDelay >= this.nbFrames - 1) {
+					this.moveFrame(this.returnTo - this.frame);
+					if (!this.loop)
+						this.pause();
+				} else {
+					this.moveFrame(nbDelay);
+					this.timePrev = time.realTime;
+				}
+			}
+		}
+	}
+};
--- Assets/Assets.js
+++ Assets/Assets.js
@@ -1,75 +1,7 @@
-// Assets ####################################
 function Assets(ctx, time) {
 	this.ctx = ctx;
 	this.time = time;
 	this.images = new this.Images();
 	this.sprites = new this.Sprites(ctx, this.images);
+	this.anims = new this.Anims(this.time, this.sprites);
 }
-
-Assets.prototype = {
-	anim: function(x, y, w, h, nbFrames, returnTo, loop, delay, imgPath) {
-		return new Assets.assetAnim(this, x, y, w, h, nbFrames, returnTo, loop, delay, imgPath);
-	}
-};
-
-// assetAnim ####################################
-Assets.assetAnim = function(assets, x, y, w, h, nbFrames, returnTo, loop, delay, imgPath) {
-	this.time = assets.time;
-	this.framesAxeX = nbFrames > 0;
-	this.nbFrames   = Math.abs(nbFrames);
-	this.returnTo   = returnTo;
-	this.loop       = loop;
-	this.delay      = delay / this.nbFrames;
-	this.frame      = this.returnTo === -1 ? -1 : 0;
-	this.sprite     = assets.sprites.create(imgPath, x, y, w, h);
-	this.timePrev   = assets.time.realTime;
-	this.pause();
-};
-Assets.assetAnim.prototype = {
-	// public
-	draw: function(x, y, debug) {
-		if (this.frame > -1) {
-			this.sprite.draw(x, y, debug);
-			this.update(this.time);
-		}
-	},
-	play: function() {
-		this.playing = true;
-		if (this.frame === -1)
-			this.moveFrame(+1);
-	},
-	pause: function() {
-		this.playing = false;
-	},
-	stop: function() {
-		this.playing = false;
-		this.moveFrame(-this.frame - (this.returnTo === -1));
-	},
-	rewind: function() {
-	},
-	// private
-	moveFrame: function(f) {
-		this.frame += f;
-		if (this.framesAxeX)
-			this.sprite.x += this.sprite.w * f;
-		else
-			this.sprite.y += this.sprite.h * f;
-	},
-	update: function(time) {
-		if (!this.playing) {
-			this.timePrev = time.realTime;
-		} else {
-			var nbDelay = Math.floor((time.realTime - this.timePrev) / this.delay);
-			if (nbDelay >= 1) {
-				if (this.frame + nbDelay >= this.nbFrames - 1) {
-					this.moveFrame(this.returnTo - this.frame);
-					if (!this.loop)
-						this.pause();
-				} else {
-					this.moveFrame(nbDelay);
-					this.timePrev = time.realTime;
-				}
-			}
-		}
-	}
-};
