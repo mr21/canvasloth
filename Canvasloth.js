08c46761c72d018c4523dca0ef55c5387ef3f78a
Mr21 <thomastortorini@gmail.com>
2014-02-16 06:47:31 -0500

    JS, la gestion des events est vachement simplifie :/

--- Canvasloth.js
+++ Canvasloth.js
@@ -1,40 +1,84 @@
-function Canvasloth(container, images, fns) {
+function Canvasloth(container, app, images) {
 	var self = this;
+	this.app = app;
 	this.canvas = document.createElement('canvas');
 	container.appendChild(this.canvas);
 	this.catchMouse = document.createElement('div');
 	this.catchMouse.className = 'canvasloth-mouse';
 	container.appendChild(this.catchMouse);
+	this.ctx = this.canvas.getContext('2d');
 	this.container = container;
-	this.ctx       = this.canvas.getContext('2d');
-	this.fns       = fns;
-	this.time      = new Time();
-	this.vectView  = new Vector2D(0, 0);
-	this.keyBool   = [];
+	this.keyBool = [];
 	this.active = false;
-	window  ._addEvent('blur',      function() { self.unfocus() });
+	window._addEvent('blur', function() { self.unfocus() });
 	document._addEvent('mousedown', function() { self.unfocus() });
+	this.vectView = new Vector2D(0, 0);
+	this.time = new Time();
 	this.pages = new Canvasloth.Pages(this);
 	this.assets = new Canvasloth.Assets(this.ctx, this.time);
 	this.assets.images.load(images, function() { self.launch() });
 }
 
 Canvasloth.prototype = {
+	setEvents: function() {
+		var self = this;
+		// Window
+		window._addEvent('resize', function() {
+			self.updateResolution();
+			self.render();
+		});
+		// Keyboard
+		if (this.app.keydown)
+			document._addEvent('keydown', function(e) {
+				if (self.active && !self.keyBool[e.keyCode]) {
+					e.preventDefault();
+					self.keyBool[e = e.keyCode] = 1;
+					self.app.keydown.call(self.app, e);
+				}
+			});
+		if (this.app.keyup)
+			document._addEvent('keyup', function(e) {
+				if (self.active &&  self.keyBool[e.keyCode]) {
+					e.preventDefault();
+					self.keyBool[e = e.keyCode] = 0;
+					self.app.keyup.call(self.app, e);
+				}
+			});
+		// Mouse
+		if (this.app.mousedown)
+			this.catchMouse._addEvent('mousedown', function(e) {
+				if (self.active)
+					self.app.mousedown.call(self.app,
+						e.layerX - self.vectView.x,
+						e.layerY - self.vectView.y
+					);
+				self.focus(e);
+			});
+		if (this.app.mouseup)
+			this.catchMouse._addEvent('mouseup', function(e) {
+				if (self.active)
+					self.app.mouseup.call(self.app,
+						e.layerX - self.vectView.x,
+						e.layerY - self.vectView.y
+					);
+			});
+		if (this.app.mousemove)
+			this.catchMouse._addEvent('mousemove', function(e) {
+				if (self.active)
+					self.app.mousemove.call(self.app,
+						e.layerX - self.vectView.x,
+						e.layerY - self.vectView.y,
+						offsetMouse.xRel,
+						offsetMouse.yRel
+					);
+			});
+	},
 	launch: function() {
 		var self = this;
-		var fns  = this.fns;
-		// Events
-		// -- keyboard
-		if (fns.keydown) document._addEvent('keydown', function(e) { if (self.active && !self.keyBool[e.keyCode]) { e.preventDefault(); self.keyBool[e = e.keyCode] = 1; fns.keydown(e) }});
-		if (fns.keyup)   document._addEvent('keyup',   function(e) { if (self.active &&  self.keyBool[e.keyCode]) { e.preventDefault(); self.keyBool[e = e.keyCode] = 0; fns.keyup  (e) }});
-		// -- mouse
-		if (fns.mousedown) this.catchMouse._addEvent('mousedown', function(e) { if (self.active) fns.mousedown(e.layerX - self.vectView.x, e.layerY - self.vectView.y); self.focus(e); });
-		if (fns.mouseup)   this.catchMouse._addEvent('mouseup',   function(e) { if (self.active) fns.mouseup  (e.layerX - self.vectView.x, e.layerY - self.vectView.y); });
-		if (fns.mousemove) this.catchMouse._addEvent('mousemove', function(e) { if (self.active) fns.mousemove(e.layerX - self.vectView.x, e.layerY - self.vectView.y, offsetMouse.xRel, offsetMouse.yRel) });
-		// -- resize
-		window._addEvent('resize', function() { self.updateResolution(); self.render(); });
+		this.setEvents();
 		this.updateResolution();
-		fns.load();
+		if (this.app.ready)
+			this.app.ready();
 		this.focus();
 		this.time.reset();
 		this.intervId = window.setInterval(function() {
@@ -51,7 +95,8 @@ Canvasloth.prototype = {
 	resetKeyboard: function() {
 		for (var i in this.keyBool)
 			if (this.keyBool[i = parseInt(i)]) {
-				this.fns.keyup(i);
+				if (this.app.keyup)
+					this.app.keyup.call(this.app, i);
 				this.keyBool[i] = 0;
 			}
 	},
@@ -78,13 +123,13 @@ Canvasloth.prototype = {
 		ctx.clearRect(0, 0, this.width(), this.height());
 		ctx.save();
 			ctx.translate(this.vectView.x, this.vectView.y);
-				this.fns.render(ctx);
+				this.app.render(ctx);
 		ctx.restore();
 	},
 	loop: function() {
 		if (this.active) {
 			this.time.update();
-			this.fns.update(this.time);
+			this.app.update(this.time);
 			this.render();
 		}
 	},
