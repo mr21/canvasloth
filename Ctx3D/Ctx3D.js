a0076b24148a79c49767b0a33105f90fdb90ec0c
Mr21 <thomastortorini@gmail.com>
2014-02-28 22:15:14 -0500

    JS, Ctx3D, setCam -> lookAt, les vecteurs de la camera ne sont plus stocke, seule la matrice l'est

--- Ctx3D/Ctx3D.js
+++ Ctx3D/Ctx3D.js
@@ -1,33 +1,33 @@
-Canvasloth.Ctx3D = function(canvas, container, ctx) {
+Canvasloth.Ctx3D = function(canvas, container) {
 	this.canvas = canvas;
 	this.ctx = canvas.getContext('webgl') || canvas.getContext("experimental-webgl");
-	this.shaders = new Canvasloth.Ctx3D.Shaders(container, ctx);
-	this.cam = {
-		'eye'    : new Canvasloth.Math.V3(0, 0, -1), // la position de la camera.
-		'center' : new Canvasloth.Math.V3(0, 0,  0), // ou regarde t-elle?
-		'up'     : new Canvasloth.Math.V3(0, 1,  0)  // l'axe roll
-	};
+	this.shaders = new Canvasloth.Ctx3D.Shaders(container, this.ctx);
+	this.camM4 = new Canvasloth.Math.M4().identity();
+	this.lookAt(
+		0,  0, -1,
+		0,  0,  0,
+		0, +1,  0
+	);
 };
 
 Canvasloth.Ctx3D.prototype = {
 	resize: function() {
 		this.canvas.resize();
 		this.ctx.viewport(0, 0, this.canvas.width(), this.canvas.height());
+		//... remettre la perspective
 	},
-	getCam: function() {
-		return this.cam;
-	},
-	setCam: function(eyeX, eyeY, eyeZ, centerX, centerY, centerZ, upX, upY, upZ) {
+	lookAt: function(eyeX, eyeY, eyeZ, centerX, centerY, centerZ, upX, upY, upZ) {
 		if (centerX === undefined) {
 			upZ     = eyeZ.z; upY     = eyeZ.y; upX     = eyeZ.x;
 			centerZ = eyeY.z; centerY = eyeY.y; centerX = eyeY.x;
 			eyeZ    = eyeX.z; eyeY    = eyeX.y; eyeX    = eyeX.x;
 		}
-		//...
+		//... modifier this.camM4
 	},
 	render: function(userApp) {
 		var c = this.ctx;
 		c.clear(c.COLOR_BUFFER_BIT | c.DEPTH_BUFFER_BIT | c.STENCIL_BUFFER_BIT);
+		//...
 		userApp.render(c);
 	}
 };
