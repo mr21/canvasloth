2d64ccb13e841909958c2ea51ae803f24cca10ae
Mr21 <thomastortorini@gmail.com>
2014-03-02 23:21:45 -0500

    JS, Ctx3D, toutes les features telles que lookAt, popMatrix, etc. sont encapsuler DANS le gl

--- Ctx3D/Ctx3D.js
+++ Ctx3D/Ctx3D.js
@@ -1,92 +1,80 @@
 Canvasloth.Ctx3D = function(canvas, container) {
+	// Creation du context
 	this.canvas = canvas;
-	// init
-	this.ctx = canvas.getContext('webgl') || canvas.getContext("experimental-webgl");
-	this.setFovy(30);
-	this.setNear(1);
-	this.setFar(10000);
-	this.setClearColor(0, 0, 0, 1);
-	this.shaders = new Canvasloth.Ctx3D.Shaders(container, this.ctx);
-	var gl = this.ctx;
+	var gl = this.ctx =
+		canvas.getContext('webgl') ||
+		canvas.getContext("experimental-webgl");
+	// Fonctionnalites additionnelles
+	// * Attributs
+	gl._shaders = new Canvasloth.Ctx3D.Shaders(container, gl);
+	gl._M4stack = [];
+	gl._M4cam = new J3DIMatrix4();
+	gl._M4obj = new J3DIMatrix4();
+	gl._M4nrm = new J3DIMatrix4();
+	gl._M4mvp = new J3DIMatrix4();
+	gl._M4nrmLoc  = gl.getUniformLocation(gl._shaders.program, 'u_normalMatrix');
+	gl._M4mdlVLoc = gl.getUniformLocation(gl._shaders.program, 'u_modelViewProjMatrix');
+	// * Matrices
+	gl._translate = function(   x, y, z) { gl._M4obj.translate(x, y, z); return this; };
+	gl._scale     = function(   x, y, z) { gl._M4obj.scale    (x, y, z); return this; };
+	gl._rotate    = function(a, x, y, z) { gl._M4obj.rotate(a, x, y, z); return this; };
+	gl._pushMatrix = function() { this._M4stack.push(new J3DIMatrix4(this._M4obj)); return this; };
+	gl._popMatrix  = function() { this._M4obj = this._M4stack.pop(); return this; };
+	// * Camera
+	gl._fovy = function(f) { this._fovy = f; };
+	gl._near = function(z) { this._near = z; };
+	gl._far  = function(z) { this._far  = z; };
+	gl._lookAt = function(eyeX, eyeY, eyeZ, centerX, centerY, centerZ, upX, upY, upZ) {
+		this._M4cam.makeIdentity();
+		this._M4cam.perspective(
+			this._fovy,
+			canvas.width() / canvas.height(),
+			this._near, this._far
+		);
+		this._M4cam.lookat(
+			eyeX,    eyeY,    eyeZ,
+			centerX, centerY, centerZ,
+			upX,     upY,     upZ
+		);
+	};
+	// * Render
+	gl._setUniform = function() {
+		// Construct the normal matrix from the model-view matrix and pass it in
+		this._M4nrm.load(this._M4obj);
+		this._M4nrm.invert();
+		this._M4nrm.transpose();
+		this._M4nrm.setUniform(this, this._M4nrmLoc, false);
+		// Construct the model-view * projection matrix and pass it in
+		this._M4mvp.load(this._M4cam);
+		this._M4mvp.multiply(this._M4obj);
+		this._M4mvp.setUniform(this, this._M4mdlVLoc, false);
+	};
+	gl._drawElements = function(mode, count, type, indices) {
+		this._setUniform();
+		this.drawElements(mode, count, type, indices);
+	};
+	// Initialiser le context
+	gl.clearColor(0, 0, 0, 1);
 	gl.enable(gl.DEPTH_TEST);
 	gl.enable(gl.BLEND);
 	gl.blendFunc(gl.SRC_ALPHA, gl.ONE_MINUS_SRC_ALPHA);
-	// Matrix
-	this.vertexTriangles = this.ctx.createBuffer();
-	this.M4stack = [];
-	this.M4cam = new J3DIMatrix4();
-	this.M4obj = new J3DIMatrix4();
-	this.M4nrm = new J3DIMatrix4();
-	this.M4mvp = new J3DIMatrix4();
-	this.M4nrmLoc = gl.getUniformLocation(this.shaders.program, 'u_normalMatrix');
-	this.M4mdlVLoc = gl.getUniformLocation(this.shaders.program, 'u_modelViewProjMatrix');
+	gl._fovy(30);
+	gl._near(1);
+	gl._far(10000);
 };
 
 Canvasloth.Ctx3D.prototype = {
-	// initialisation
-	setClearColor: function(r, g, b, a) {
-		this.ctx.clearColor(r, g, b, a);
-	},
-	setFovy: function(fovy) {
-		this.fovy = fovy;
-	},
-	setNear: function(near) {
-		this.near = near;
-	},
-	setFar: function(far) {
-		this.far = far;
-		this.ctx.clearDepth(far);
-	},
 	resize: function() {
 		this.canvas.resize();
 		var w = this.canvas.width(),
 		    h = this.canvas.height();
 		this.ctx.viewport(0, 0, w, h);
 	},
-	// camera
-	lookAt: function(eyeX, eyeY, eyeZ, centerX, centerY, centerZ, upX, upY, upZ) {
-		this.M4cam.makeIdentity();
-		this.M4cam.perspective(
-			this.fovy,
-			this.canvas.width() / this.canvas.height(),
-			this.near, this.far
-		);
-		this.M4cam.lookat(
-			eyeX,    eyeY,    eyeZ,
-			centerX, centerY, centerZ,
-			upX,     upY,     upZ
-		);
-	},
-	// transform
-	translate: function(   x, y, z) { this.M4obj.translate(x, y, z); return this; },
-	scale:     function(   x, y, z) { this.M4obj.scale    (x, y, z); return this; },
-	rotate:    function(a, x, y, z) { this.M4obj.rotate(a, x, y, z); return this; },
-	pushMatrix: function() { this.M4stack.push(new J3DIMatrix4(this.M4obj)); return this; },
-	popMatrix : function() { this.M4obj = this.M4stack.pop(); return this; },
 	// render
-	setUniform: function() {
-		// Construct the normal matrix from the model-view matrix and pass it in
-		this.M4nrm.load(this.M4obj);
-		this.M4nrm.invert();
-		this.M4nrm.transpose();
-		this.M4nrm.setUniform(this.ctx, this.M4nrmLoc, false);
-		// Construct the model-view * projection matrix and pass it in
-		this.M4mvp.load(this.M4cam);
-		this.M4mvp.multiply(this.M4obj);
-		this.M4mvp.setUniform(this.ctx, this.M4mdlVLoc, false);
-	},
-	drawElements: function(mode, count, type, indices) {
-		this.setUniform();
-		this.ctx.drawElements(mode, count, type, indices);
-	},
 	render: function(userApp) {
 		var gl = this.ctx;
 		gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT | gl.STENCIL_BUFFER_BIT);
-		this.M4obj.makeIdentity();
-
-		userApp.render(this);
-
-		/*gl.bindBuffer(gl.ARRAY_BUFFER, this.vertexTriangles);
-		gl.drawElements(gl.TRIANGLES, this.vertexTriangles.length, gl.FLOAT, 0);*/
+		gl._M4obj.makeIdentity();
+		userApp.render(gl);
 	}
 };
