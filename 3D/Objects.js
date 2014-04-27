0161a22dd1fd00e2b167419878c0fdbcfa975b01
Mr21 <thomastortorini@gmail.com>
2014-04-27 05:00:37 -0400

    Objects, le bind est appelle dans le draw

--- 3D/Objects.js
+++ 3D/Objects.js
@@ -4,58 +4,53 @@ Canvasloth.prototype.Objects3D = function() {
 	    shaders = this.shaders,
 	    matrix = this.matrix;
 	this.objects = {
-		create: function(obj) {
-			return new Canvasloth.Object(this, gl, obj);
+		create: function(data) {
+			var obj = new Canvasloth.Object(this);
+			obj.vtxBuf = gl.createBuffer(); obj.vtxNb = data.vtx.length / 3;
+			obj.nrmBuf = gl.createBuffer(); obj.nrmNb = data.nrm.length / 3;
+			obj.texBuf = gl.createBuffer(); obj.texNb = data.tex.length / 2;
+			obj.colBuf = gl.createBuffer(); obj.colNb = data.col.length / 4;
+			obj.indBuf = gl.createBuffer(); obj.indNb = data.ind.length / 1;
+			gl.bindBuffer(gl.ARRAY_BUFFER,         obj.vtxBuf); gl.bufferData(gl.ARRAY_BUFFER,         new Float32Array(data.vtx), gl.STATIC_DRAW);
+			gl.bindBuffer(gl.ARRAY_BUFFER,         obj.nrmBuf); gl.bufferData(gl.ARRAY_BUFFER,         new Float32Array(data.nrm), gl.STATIC_DRAW);
+			gl.bindBuffer(gl.ARRAY_BUFFER,         obj.texBuf); gl.bufferData(gl.ARRAY_BUFFER,         new Float32Array(data.tex), gl.STATIC_DRAW);
+			gl.bindBuffer(gl.ARRAY_BUFFER,         obj.colBuf); gl.bufferData(gl.ARRAY_BUFFER,         new Uint8Array  (data.col), gl.STATIC_DRAW);
+			gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, obj.indBuf); gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Uint16Array (data.ind), gl.STATIC_DRAW);
+			return obj;
 		},
-		_setUniform: function() {
-			gl.uniformMatrix4fv(shaders.uPMatrix, false, matrix.p);
-			gl.uniformMatrix4fv(shaders.uMVMatrix, false, matrix.m);
+		draw: function(obj) {
+			// bind
+			if (this._currentObj !== obj) {
+				this._currentObj = obj;
+				gl.bindBuffer(gl.ARRAY_BUFFER,         obj.vtxBuf); gl.vertexAttribPointer(2, 3 /*itemsize*/, gl.FLOAT,         false, 0, 0);
+				gl.bindBuffer(gl.ARRAY_BUFFER,         obj.nrmBuf); gl.vertexAttribPointer(0, 3 /*itemsize*/, gl.FLOAT,         false, 0, 0);
+				gl.bindBuffer(gl.ARRAY_BUFFER,         obj.colBuf); gl.vertexAttribPointer(1, 4 /*itemsize*/, gl.UNSIGNED_BYTE, true,  0, 0);
+				gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, obj.indBuf);
+			}
+			// setUniform
 			matrix.n = mat4.clone(matrix.m);
 			mat4.invert(matrix.n, matrix.n);
 			mat4.transpose(matrix.n, matrix.n);
-			gl.uniformMatrix4fv(shaders.uNMatrix, false, matrix.n);
+			gl.uniformMatrix4fv(shaders.uNMatrix,  false, matrix.n);
+			gl.uniformMatrix4fv(shaders.uPMatrix,  false, matrix.p);
+			gl.uniformMatrix4fv(shaders.uMVMatrix, false, matrix.m);
+			// draw
+			gl.drawElements(
+				gl.TRIANGLES,
+				obj.indNb,
+				gl.UNSIGNED_SHORT,
+				0
+			);
 		}
 	};
 };
 
-Canvasloth.Object = function(objects, gl, obj) {
-	this.objects = objects;
-	this.gl = gl;
-
-	this.vtxBuf = gl.createBuffer(); this.vtxNb = obj.vtx.length / 3;
-	this.nrmBuf = gl.createBuffer(); this.nrmNb = obj.nrm.length / 3;
-	this.texBuf = gl.createBuffer(); this.texNb = obj.tex.length / 2;
-	this.colBuf = gl.createBuffer(); this.colNb = obj.col.length / 4;
-	this.indBuf = gl.createBuffer(); this.indNb = obj.ind.length / 1;
-
-	gl.bindBuffer(gl.ARRAY_BUFFER,         this.vtxBuf); gl.bufferData(gl.ARRAY_BUFFER,         new Float32Array(obj.vtx), gl.STATIC_DRAW);
-	gl.bindBuffer(gl.ARRAY_BUFFER,         this.nrmBuf); gl.bufferData(gl.ARRAY_BUFFER,         new Float32Array(obj.nrm), gl.STATIC_DRAW);
-	gl.bindBuffer(gl.ARRAY_BUFFER,         this.texBuf); gl.bufferData(gl.ARRAY_BUFFER,         new Float32Array(obj.tex), gl.STATIC_DRAW);
-	gl.bindBuffer(gl.ARRAY_BUFFER,         this.colBuf); gl.bufferData(gl.ARRAY_BUFFER,         new Uint8Array  (obj.col), gl.STATIC_DRAW);
-	gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, this.indBuf); gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Uint16Array (obj.ind), gl.STATIC_DRAW);
+Canvasloth.Object = function(parent) {
+	this.parent = parent;
 };
 
 Canvasloth.Object.prototype = {
-	bind: function() {
-		if (this.objects._currentObj !== this) {
-			var gl = this.gl;
-			this.objects._currentObj = this;
-			gl.bindBuffer(gl.ARRAY_BUFFER,         this.vtxBuf); gl.vertexAttribPointer(2, 3 /*itemsize*/, gl.FLOAT,         false, 0, 0);
-			gl.bindBuffer(gl.ARRAY_BUFFER,         this.nrmBuf); gl.vertexAttribPointer(0, 3 /*itemsize*/, gl.FLOAT,         false, 0, 0);
-			gl.bindBuffer(gl.ARRAY_BUFFER,         this.colBuf); gl.vertexAttribPointer(1, 4 /*itemsize*/, gl.UNSIGNED_BYTE, true,  0, 0);
-			gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, this.indBuf);
-		}
-		return this;
-	},
 	draw: function() {
-		var gl = this.gl;
-		this.objects._setUniform();
-		gl.drawElements(
-			gl.TRIANGLES,
-			this.indNb,
-			gl.UNSIGNED_SHORT,
-			0
-		);
-		return this;
+		return this.parent.draw(this), this;
 	}
 };
