ac8534b78122e1161ad74a027c55786fc26e61f7
Mr21 <thomastortorini@gmail.com>
2014-04-29 01:49:02 -0400

    Grid, de meilleures couleurs visible peu importe la couleur de fond

--- 3D/Grid.js
+++ 3D/Grid.js
@@ -1,25 +1,27 @@
 Canvasloth.prototype.Grid3D = function() {
 	var lights = this.lights,
 		nbLines = 11,
-		colorGray = 255,
 		ind = [], vtx = [], nrm = [],
 		col = [], tex = [], alpha,
-		size = parseInt(nbLines / -2);
+		size = parseInt(nbLines / -2),
+		i, coord,
+		r, g, b, a;
 
-	for (var i = 0, coord = size; i < nbLines; ++i, ++coord)
+	for (i = 0, coord = size; i < nbLines; ++i, ++coord)
 		vtx.push(
 			coord,-size,0,   coord,+size,0,
 			-size,coord,0,   +size,coord,0
 		);
 	nbLines *= 2;
-	for (var i = 0; i < nbLines; ++i) {
+	for (i = 0; i < nbLines; ++i) {
 		ind.push(i*2,   i*2+1);
 		nrm.push(0,0,1, 0,0,1);
 		tex.push(0,0,   0,0);
-		alpha = i === nbLines/2 || i === nbLines/2-1 ? 24 : 4;
+		if (i % 2) { r=  0; g=0; b=196; a = i === nbLines/2   ? 255 : 64; }
+		else       { r=196; g=0; b=  0; a = i === nbLines/2-1 ? 255 : 64; }
 		col.push(
-			colorGray, colorGray, colorGray, alpha,
-			colorGray, colorGray, colorGray, alpha
+			r, g, b, a,
+			r, g, b, a
 		);
 	}
 
