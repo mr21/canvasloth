d3d085fe6f2a63b1c518bddf68802b560d08b2ee
Mr21 <thomastortorini@gmail.com>
2015-01-22 06:27:17 +0100

    addon: anim

---
+++ src/addons/canvasloth-anim.js
@@ -0,0 +1,75 @@
+/*
+	Canvasloth Animation - 1.0
+	https://github.com/Mr21/Canvasloth
+*/
+
+'use strict';
+
+function canvaslothAnim(sprite) {
+	this.sprite = sprite;
+	this.timer = 0;
+	this.currFrame = 0;
+	this
+		.play()
+		.nbFrames(1)
+		.duration(1)
+		.frame(0)
+		.loopAt(0)
+		.looping(false)
+}
+
+canvaslothAnim.prototype = {
+	play: function() {
+		this.isPlaying = true;
+		return this;
+	},
+	pause: function() {
+		this.isPlaying = false;
+		return this;
+	},
+	looping: function(l) {
+		this.isLooping = l;
+		return this;
+	},
+	loopAt: function(n) {
+		this.frameLoop = n;
+		return this;
+	},
+	duration: function(sec) {
+		this.sec = sec;
+		this.secByFrame = sec / this.nbFrm;
+		return this;
+	},
+	nbFrames: function(nb) {
+		this.nbFrm = nb;
+		this.secByFrame = this.sec / nb;
+		return this;
+	},
+	frame: function(n) {
+		if (n >= this.nbFrm) {
+			if (this.isLooping) {
+				n = this.frameLoop;
+			} else {
+				n = 0;
+				this.pause();
+			}
+		}
+		if ((n %= this.nbFrm) < 0)
+			n += this.nbFrm;
+		this.sprite.srcPos(
+			this.sprite.sx + (n - this.currFrame) * this.sprite.sw,
+			this.sprite.sy
+		);
+		this.currFrame = n;
+		return this;
+	},
+	draw: function(x, y, frameTime) {
+		if ((this.timer += frameTime) >= this.secByFrame) {
+			if (this.isPlaying)
+				this.frame(this.currFrame + Math.floor(this.timer / this.secByFrame));
+			this.timer = 0;
+		}
+		this.sprite.draw(x, y);
+		return this;
+	}
+};
